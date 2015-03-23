afc8e471146d8033c45add7f80fa56a3b1edc355
Nyk Ma <moe@nayuki.info>
2015-03-23 16:01:21 +0800

    Fix push message structure

--- lib/xinge/ios.rb
+++ lib/xinge/ios.rb
@@ -8,31 +8,33 @@ module Xinge
       super
     end
 
-    def pushToSingleAccount(account, title, content, params = {})
-      self.push_single_account(account, 1, build_simple_message(title, content), params.merge({environment: ENV_MAP[Xinge.config[:env]]}))
+    def pushToSingleAccount(account, title, content, badge = 1, params = {})
+      self.push_single_account(account, 1, build_simple_message(title, content, badge), params.merge({environment: ENV_MAP[Xinge.config[:env]]}))
     end
 
-    def pushToSingleDevice(token, title, content, params = {})
-      self.push_single_device(token, 1, build_simple_message(title, content), params.merge({environment: ENV_MAP[Xinge.config[:env]]}))
+    def pushToSingleDevice(token, title, content, badge = 1, params = {})
+      self.push_single_device(token, 1, build_simple_message(title, content, badge), params.merge({environment: ENV_MAP[Xinge.config[:env]]}))
     end
 
-    def pushToAllDevice(title, content, params = {})
-      self.push_all_device(1, build_simple_message(title, content), params.merge({environment: ENV_MAP[Xinge.config[:env]]}))
+    def pushToAllDevice(title, content, badge = 1, params = {})
+      self.push_all_device(1, build_simple_message(title, content, badge), params.merge({environment: ENV_MAP[Xinge.config[:env]]}))
     end
 
     protected
 
-    def build_simple_message(title,content)
+    def build_simple_message(title, content, badge = 1)
       result = {
         aps: {
-          alert: {
-            body: content
-          },
           sound: 'default',
-          badge: 5
+          badge: badge
         }
       }
-      result[:aps][:alert][:title] = title unless title.empty?
+      if title.empty?
+        result[:aps][:alert] = content[:alert]
+      else
+        result[:aps][:alert] = {title: title, body: content[:alert]}
+      end
+      result.merge!(content[:custom_content]) unless content[:custom_content].nil?
       result.to_json
     end
   end
